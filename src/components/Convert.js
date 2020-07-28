d70c8017e23c9067243bad9880b5f3509a4de575
iggy-pigy <ilgakokorisa@gmail.com>
2020-07-28 18:09:14 +0100

    Created Convert component

---
+++ src/components/Convert.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class Convert extends Component {
+    render() {
+        return (
+            <du></du>
+        )
+    }
+};
+
+export default Convert;
