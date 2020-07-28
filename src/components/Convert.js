41eaaf755c554c67918195d9aeb30a205794ddc2
iggy-pigy <ilgakokorisa@gmail.com>
2020-07-28 18:17:12 +0100

    Added state to Convert component

--- src/components/Convert.js
+++ src/components/Convert.js
@@ -1,6 +1,11 @@
 import React, { Component } from 'react';
 
 class Convert extends Component {
+    constructor() {
+        super();
+
+        this.state = { letters: '' };
+    }
     render() {
         return (
             <du></du>
